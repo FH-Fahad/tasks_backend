9f3603e82ebd9e5b6a2a33713f60f4e91f96d4b4
fh-fahad <fahadbin8900@gmail.com>
2024-01-04 21:22:17 +0600

    add done

---
+++ index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const userRouter = require("./routes/user");
+const taskRouter = require("./routes/task");
+
+const app = express();
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  })
+);
+
+app.use("/api/user", userRouter);
+app.use("/api/tasks", taskRouter);
+
+const port = process.env.PORT || 4000;
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
